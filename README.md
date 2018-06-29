4dbfd0a3f5fe7b438a1a3f58714a5c7fd52456ee
Ricky Peters <ericrickyallenpeters@gmail.com>
2018-06-28 18:51:50 -0700

    Initial Webpack Setup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Name-Generator
-SPA project to generate punk rock band names
+##SPA project to generate punk rock band names.
+Type in your first name and your last name's initial to receive your punk rock name.
---
+++ config/webpack.base.config.js
@@ -0,0 +1,30 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader!sass-loader",
+        })
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    }),
+    new ExtractTextPlugin('styles.css')
+  ]
+};
